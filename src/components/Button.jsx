5a8dbfcae94abf8a3a74314644d0fc9156cab98f
tecni2 <tecni2.elie.elie@gmail.com>
2022-10-25 14:25:03 -0500

    added the responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 
+
 const Button = ({action, color}) => {
  
   return (
 
       <button id="circle" onClick={action}>
         {/* <i className ="fa-sharp fa-solid fa-arrows-to-circle" style={{ color: colors[randomColorIndex]}} ></i> */}
-        <i className ={`fa-sharp fa-solid fa-arrows-to-circle ${color}`} style={{color: color}} ></i>
+        <i className ="fa-sharp fa-solid fa-arrows-to-circle" style={{color: color}} ></i>
         
        </button> 
   );
